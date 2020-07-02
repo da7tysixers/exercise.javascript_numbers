9e041b1879e92d49ca1ff934dc4713f1f1a7934d
Emmanuel Orubele <blontf@gmail.com>
2020-07-01 23:17:22 -0400

    Changes to the file and corrected my assignment

--- assets/js/numbers.js
+++ assets/js/numbers.js
@@ -1,3 +1,17 @@
 function compute(expression) {
     // TODO - write method definition here
+    // let arr = [];
+    // for (let i = 0; i < expression.length; i++) {
+    //     arr[i] = expression.charAt();
+
+    // }
+
+    // return console.log(arr[1]);
+
+
+    // this single line of code solves the headache.
+    let arr = eval(expression);
+    return arr;
+
+
 }
